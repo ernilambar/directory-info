56e6b59e994e222387477d932fe67489beb1052b
Nilambar Sharma <nilambar@outlook.com>
2022-09-29 11:59:38 +0545

    feat: dashboard

--- directory-info.php
+++ directory-info.php
@@ -3,7 +3,7 @@
  * Plugin Name: Directory Info
  * Plugin URI: https://github.com/ernilambar/directory-info/
  * Description: Info of themes and plugins.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Nilambar Sharma
  * Author URI: https://www.nilambar.net/
  * Text Domain: directory-info
@@ -20,16 +20,25 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'DIRECTORY_INFO_VERSION', '1.0.1' );
+define( 'DIRECTORY_INFO_VERSION', '1.0.2' );
 define( 'DIRECTORY_INFO_SLUG', 'directory-info' );
 define( 'DIRECTORY_INFO_BASENAME', plugin_basename( __FILE__ ) );
 define( 'DIRECTORY_INFO_BASE_FILENAME', plugin_basename( __FILE__ ) );
 define( 'DIRECTORY_INFO_DIR', rtrim( plugin_dir_path( __FILE__ ), '/' ) );
 define( 'DIRECTORY_INFO_URL', rtrim( plugin_dir_url( __FILE__ ), '/' ) );
 
+if ( ! defined( 'WP_WELCOME_DIR' ) ) {
+	define( 'WP_WELCOME_DIR', DIRECTORY_INFO_DIR . '/vendor/ernilambar/wp-welcome' );
+}
+
+if ( ! defined( 'WP_WELCOME_URL' ) ) {
+	define( 'WP_WELCOME_URL', DIRECTORY_INFO_URL . '/vendor/ernilambar/wp-welcome' );
+}
+
 // Include autoload.
 if ( file_exists(  DIRECTORY_INFO_DIR . '/vendor/autoload.php' ) ) {
 	require_once DIRECTORY_INFO_DIR . '/vendor/autoload.php';
+	require_once DIRECTORY_INFO_DIR . '/vendor/ernilambar/wp-welcome/init.php';
 }
 
 if ( class_exists( 'DirectoryInfo\Init' ) ) {
